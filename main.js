242a2ebfa50a38cb3c861800087d11f3aecf96ec
Jozef <jozefw@sbcglobal.net>
2015-04-14 15:58:48 -0700

    add functionality

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
-// clears the board
-$('#container').html("");
-var food = "";
-var tail= [];
-// make rows and coloumns for the board
-for ( var row = 0; row < 20; row++ ) {
-	for ( var column = 0; column < 20; column++ ) {
-		$('#container').append('<div id="columns_'+row+'_'+column+'" class = "boardCell"</div>');
-	}
-}
-
-// this is the snake position
 (function init() {
+	// clears the board
+	$('#container').html("");
+	var food = "",
+			direction = 1,
+			food = "",
+			snake = ["3_10", "2_10","1_10"],
+			tail= [];
+
+// make rows and coloumns for the board
+	for ( var row = 0; row < 20; row++ ) {
+		for ( var column = 0; column < 20; column++ ) {
+			$('#container').append('<div id="columns_'+row+'_'+column+'" class = "boardCell"</div>');
+		}
+	}
+
+// make the snake position
 	$('#columns_1_10').addClass('snakeCell');
 	$('#columns_2_10').addClass('snakeCell');
 	$('#columns_3_10').addClass('snakeCell');
+
+	// put some food on the board
+	generateFood();
+// run gameUpdate to get the snake moving
+	setTimeout( function(){ gameUpdate}, speed );
 })();
 
-(function generateFood() {
+
+function generateFood() {
 	var appleRow = Math.floor(Math.random() *19 );
 	var appleColumn = Math.floor(Math.random()* 19 );
 	$('#columns_'+appleRow+'_'+appleColumn).addClass('appleCell');
 	food = row+"_"+column;
-})();
+};
 
-function snakeTail(){
-	// you want the array with just the rows & colums not the id name
-	var snake = ["3_10", "2_10","1_10"];
+function gameUpdate() {
 	var tail = snake.pop();
 	$('#columns_'+tail).removeClass('appleCell');
-}
 
-function snakeMove() {
 	// define the snake head, placement of the snakes head will point to the direction of movement
 	var head = snake[0],
 	// the direction of movement is going to be either into a row or a column so we need to break the head up into these component parts
@@ -52,8 +58,9 @@ function snakeMove() {
 		case 4:
 			column = column + 1 // move to right
 			break;
+}
 
-	}
+
 	// snake direction IS the placement of a new cell in front of the head in the desired direction from the keypress above 
 	var newSnakeDir = row+"_"+column;
 	snake.unshift(newSnakeDir);
